9d8a0f142163ffef1ec8e0b03aa6d557c1546f13
gfhjgdykgu <gfhjgdykgu@LAPTOP-TLUQVMFP>
2023-10-05 17:56:58 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Threading.Channels;
+
+namespace ehliyetSinavi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region kaldınmı gectinmi uyg.
+            //Console.Write("final notunu giriniz:  ");
+            //int final = int.Parse(Console.ReadLine());
+            //Console.Write("1. vize notunu giriniz: ");
+            //int vize1 = int.Parse(Console.ReadLine());
+            //Console.Write("2. vize notunu giriniz: ");
+            //int vize2 = int.Parse(Console.ReadLine());
+            //
+            //int sonuc1 = final * 60 / 100;
+            //int sonuc2 = vize1 * 20 / 100;
+            //int sonuc3 = vize2 * 20 / 100;
+            //int toplam = sonuc1 + sonuc2 + sonuc3;
+            //
+            //if (toplam >= 90 )
+            //{
+            //    Console.WriteLine("AA aldınız , harika");
+            //}
+            //else if (toplam >=85 )
+            //{
+            //    Console.WriteLine("BA aldınız, çok iyi");
+            //}
+            //else if (toplam >= 80)
+            //{
+            //    Console.WriteLine("BB aldınız , iyi");
+            //}
+            //else if (toplam >= 70)
+            //{
+            //    Console.WriteLine("CB aldınız , orta-iyi");
+            //}
+            //else if (toplam >= 60)
+            //{
+            //    Console.WriteLine("CC aldınız , yeterli");
+            //}
+            //else if (toplam >= 50)
+            //{
+            //    Console.WriteLine("DC aldınız , geçmek için yeterli - şartlı geçiş");
+            //}
+            //else if (toplam >= 45)
+            //{
+            //    Console.WriteLine("DD aldınız , geçti - şartlı geçiş");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("FF aldınız , kaldınız");
+            //}
+            #endregion
+            #region 100 e kadar tek sayıları yzd. uyg.
+
+            
+            //for (int sayi = 1; sayi < 100; sayi++)
+            //{
+            //    if (sayi % 2 == 0)
+            //        continue;
+            //    Console.WriteLine(sayi);
+            //
+            //}
+            #endregion
+        }
+    }
+}
